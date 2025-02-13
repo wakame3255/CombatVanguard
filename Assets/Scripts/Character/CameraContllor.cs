0c5a53cc416f468a9d5a00f050838005c260bc9b
wakame3255 <46024199+wakame3255@users.noreply.github.com>
2025-02-13 14:56:40 +0900

    カメラの更新

--- Assets/Scripts/Character/CameraContllor.cs
+++ Assets/Scripts/Character/CameraContllor.cs
@@ -11,5 +11,35 @@ public class CameraContllor : MonoBehaviour
     [SerializeField]
     private CinemachineVirtualCamera _lockOnCamera;
 
+    [SerializeField]
+    private Transform _PlayerPos;
 
+    [SerializeField]
+    private LayerMask _hitLayerMask;
+
+    [SerializeField]
+    private int _raycastHitCount;
+
+    [SerializeField]
+    private float _distance; 
+
+    private RaycastHit[] _raycastHits;
+
+    private void Awake()
+    {
+        _raycastHits = new RaycastHit[_raycastHitCount];
+    }
+
+    private void Update()
+    {
+        Vector3 cameraDirection = (_PlayerPos.position - _mainCamera.transform.position);
+
+        int hitCount = Physics.BoxCastNonAlloc
+            (_mainCamera.transform.position, Vector3.forward * _distance, cameraDirection.normalized, _raycastHits, Quaternion.identity, _distance, _hitLayerMask);
+
+        for (int i = 0; i < hitCount; i++)
+        {
+            print(_raycastHits[i].collider.name);
+        }
+    }
 }
